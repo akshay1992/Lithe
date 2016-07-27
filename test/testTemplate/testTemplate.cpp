cf3c2899f99751e4eb10c386ca119d3f58bf4b01
akshay1992 <akshay19.92@gmail.com>
2016-07-26 22:31:23 -0700

    Added newTest script along with template test

---
+++ test/testTemplate/testTemplate.cpp
@@ -0,0 +1,15 @@
+#include "testTemplate.h"
+#include "Block.h"
+
+TemplateTest::TemplateTest(void) {}
+
+TemplateTest::~TemplateTest(void) {}
+
+void TemplateTest::SetUp(void) {}
+
+void TemplateTest::TearDown(void) {}
+
+TEST_F(TemplateTest, removeThisTest)
+{
+    EXPECT_EQ(1, 2);
+}
