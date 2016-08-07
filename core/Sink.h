aa9ec6fae41f59838429815bae4aff6c744978ed
akshay1992 <akshay19.92@gmail.com>
2016-08-07 03:07:05 -0700

    Imported built allocore Added it as a submodule in core/ Modified Sink.h to see if it links. Modified core/CMakelists.txt to link to allocore

--- core/Sink.h
+++ core/Sink.h
@@ -3,14 +3,41 @@
 
 #include "Block.h"
 
+#include "allocore/sound/al_AudioScene.hpp"
+#include "allocore/sound/al_StereoPanner.hpp"
+
 class Sink : public Block
 {
 public:
-	Sink(int nInputs) : Block(nInputs, 0) {}
+	Sink(int nInputs, int block_size = 512) 
+		: 
+		Block(nInputs, 0) ,
+		audio_block_size(512)
+	{
+		speaker_layout = al::HeadsetSpeakerLayout();
+		panner = new al::StereoPanner(speaker_layout);
+		scene = new al::AudioScene(audio_block_size);
+
+		for(int i=0; i<nInputs; i++)
+		{
+			sources.push_back( new al::SoundSource() );
+			scene->addSource( *sources[i] );
+		}
+
+		listener = scene->createListener(panner);
+		listener->compile();
+	}
 
 private:
 	using Block::getNumOutputs;
 	using Block::getOutputs;
+
+	int audio_block_size;
+	al::StereoPanner *panner;
+	al::AudioScene* scene;
+	al::Listener* listener;
+	al::SpeakerLayout speaker_layout;
+	std::vector<al::SoundSource* > sources;
 };
 
 #endif
