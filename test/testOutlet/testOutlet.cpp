a746167bc7240b9524cda3d011501dd8d52a7604
akshay1992 <akshay19.92@gmail.com>
2018-04-29 17:17:31 -0700

    test was written wrong. Expected wrong results.

--- test/testOutlet/testOutlet.cpp
+++ test/testOutlet/testOutlet.cpp
@@ -11,15 +11,15 @@ void OutletTest::SetUp(void) {}
 
 void OutletTest::TearDown(void) {}
 
-// TEST_F(OutletTest, constructorTest)
-// {
-// 	Node node1(1, 1);
-// 	Node node2(1, 1);
+TEST_F(OutletTest, constructorTest)
+{
+	Node node1(1, 1);
+	Node node2(1, 1);
 
-// 	EXPECT_EQ ( node1.getOutlet(0).getParentNode(), &node1 );
+	EXPECT_EQ ( node1.getOutlet(0).getParentNode(), &node1 );
 
-// 	EXPECT_NE ( node1.getOutlet(0).getParentNode(), &node2 );
-// }
+	EXPECT_NE ( node1.getOutlet(0).getParentNode(), &node2 );
+}
 
 TEST_F(OutletTest, isConnected)
 {
@@ -46,24 +46,24 @@ TEST_F(OutletTest, isConnected)
 		EXPECT_TRUE( outlet.getConnectedInlets()[2] == &i2 );
 
 		outlet.disconnect(i1);
-		EXPECT_TRUE(i1.isConnected());
-		EXPECT_EQ(outlet.getConnectedInlets().size(), 3);
+		EXPECT_FALSE(i1.isConnected());
+		EXPECT_EQ(outlet.getConnectedInlets().size(), 2);
 	}
 }
 
-// TEST_F(OutletTest, get_set_Sample)
-// {
-// 	Node node(1, 1);
-// 	EXPECT_EQ( node.getOutlet().getSample().audio, 0);
-// 	EXPECT_EQ( node.getOutlet().getSample().az, 0);
-// 	EXPECT_EQ( node.getOutlet().getSample().el, 0);
-// 	EXPECT_EQ( node.getOutlet().getSample().d, 0);
+TEST_F(OutletTest, get_set_Sample)
+{
+	Node node(1, 1);
+	EXPECT_EQ( node.getOutlet().getSample().audio, 0);
+	EXPECT_EQ( node.getOutlet().getSample().az, 0);
+	EXPECT_EQ( node.getOutlet().getSample().el, 0);
+	EXPECT_EQ( node.getOutlet().getSample().d, 0);
 
-// 	Sample s(0.1, 0.3, -.7, -1.0);
-// 	node.getOutlet().setSample( s );
+	Sample s(0.1, 0.3, -.7, -1.0);
+	node.getOutlet().setSample( s );
 
-// 	EXPECT_EQ( node.getOutlet().getSample().audio, s.audio);
-// 	EXPECT_EQ( node.getOutlet().getSample().az, s.az);
-// 	EXPECT_EQ( node.getOutlet().getSample().el, s.el);
-// 	EXPECT_EQ( node.getOutlet().getSample().d, s.d);
-// }
+	EXPECT_EQ( node.getOutlet().getSample().audio, s.audio);
+	EXPECT_EQ( node.getOutlet().getSample().az, s.az);
+	EXPECT_EQ( node.getOutlet().getSample().el, s.el);
+	EXPECT_EQ( node.getOutlet().getSample().d, s.d);
+}
