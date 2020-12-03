2a5cf25eeb44ea4bb5ba7e24406690ec9a34c0d0
Akshay <akshaycadambi@gmail.com>
2020-12-02 20:14:39 -0800

    Fixed the returning pointer to temporary in the exceptions

--- Lithe/Exceptions.h
+++ Lithe/Exceptions.h
@@ -9,72 +9,29 @@ namespace lithe{
 class InvalidInletException : public std::range_error
 {
 public:
-    InvalidInletException(int index, int nodeID) : std::range_error("Inlet not found"), inletIndex(index), nodeID(nodeID) {}
-    virtual const char* what() const throw()
-    {
-        message.str( "" );
-        message << std::range_error::what() << "at Index:  " << std::to_string(inletIndex) <<  " NodeID: " << std::to_string(nodeID);
-        return message.str().c_str();
-    }
-
-private:
-	int nodeID;
-    int inletIndex;
-    static std::ostringstream message;
+    InvalidInletException(int index, int nodeID)
+        : std::range_error("Inlet not found at Index:  " + std::to_string(index) + " NodeID: " + std::to_string(nodeID)) {}
 };
 
 class InvalidOutletException : public std::range_error
 {
 public:
-    InvalidOutletException(int index, int nodeID) : std::range_error("Outlet not found"), outletIndex(index), nodeID(nodeID) {}
-    virtual const char* what() const throw()
-    {
-        message.str( "" );
-        message << std::range_error::what() << "at Index:  " << std::to_string(outletIndex) <<  " NodeID: " << std::to_string(nodeID);
-        return message.str().c_str();
-    }
-
-private:
-	int nodeID;
-    int outletIndex;
-    static std::ostringstream message;
+    InvalidOutletException(int index, int nodeID)
+        : std::range_error("Outlet not found at Index:  " + std::to_string(index) +  " NodeID: " + std::to_string(nodeID)) {}
 };
 
 class NodeNotFoundException : public std::range_error
 {
 public:
-    NodeNotFoundException(int id) : std::range_error("Node not found"), nodeID(id) {}
-    virtual const char* what() const throw()
-    {
-        message.str( "" );
-        message << std::range_error::what() << "   ID: " << std::to_string(nodeID);
-        return message.str().c_str();
-    }
-
-private:
-    int nodeID;
-    static std::ostringstream message;
+    NodeNotFoundException(int id)
+        : std::range_error("Node not found   ID: " + std::to_string(id)) {}
 };
 
 class RangeIsZeroError : public std::range_error
 {
 public:
     RangeIsZeroError(float max, float min) : 
-        std::range_error("RangeT initialization error: Range can't be zero "), 
-        max(max), 
-        min(min) 
-    {}
-
-    virtual const char* what() const throw()
-    {
-        message.str( "" );
-        message << std::range_error::what() << " Provided max: " << max <<  " min: " << min;
-        return message.str().c_str();
-    }
-
-private:
-    float max, min;
-    static std::ostringstream message;
+        std::range_error("RangeT initialization error: Range can't be zero  Provided max: " + std::to_string(max) +  " min: " + std::to_string(min)) {}
 };
 
 }; // namespace lithe
